8b4328936454f6f40cb7e01d91796adf8526c802
sugardiri <115659570+sugardiri@users.noreply.github.com>
2022-12-08 06:42:18 -0500

    lightbox

---
+++ Projects/Lightbox/js/lightbox.js
@@ -0,0 +1,33 @@
+//Variable for lightbox elements
+let lightboxContainer = document.getElementById ('lightboxContainer');
+let lightboxContainer = document.getElementById ('lightboxBackground');
+let lightbox = document.getElementbyId ('lightbox');
+let lightbox = document.getElementId ('lightboxCloser');
+
+//Function to open lightbox
+function openLightbox(){
+//turn on display class (from displays:none to display:flex)
+lightboxContainer.classList.add('display');
+//Display image scr inside lightbox
+lightbox.scr = this.scr;
+}
+
+let coffeeImage = document.getElementById ('coffeeImage1');
+
+
+
+//Function to Open lightbox
+
+//media MediaQueryList
+//how to conenct java ScriptProcessor
+//connect css framework
+//difference in line and block Elements
+//50 questions
+//making website second parent
+//bootstrap style guide 
+//any notes
+//parts of other projects
+
+
+
+

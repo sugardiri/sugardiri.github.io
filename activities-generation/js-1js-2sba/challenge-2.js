cd431a3de998ceef7dc5104dc7c9e995f923e7dc
sugardiri <jgonzalez731995@gmail.com>
2023-05-15 09:40:18 -0500

    commit

---
+++ activities-generation/js-1js-2sba/challenge-2.js
@@ -0,0 +1,31 @@
+// Below are the items available for sale at a local computer store:
+
+// computer = $500
+// mouse = $10
+// tablet = $250
+// case = $25
+// router = $100
+
+// Write a function called itemPrice that is defined with 1 parameter: item
+// The function will use a switch statement to return the price of the item passed in.
+// Check to make sure that all arguments passed in are of data type string
+// If an argument is not of data type string, a message should be logged to the console informing the user that all entries must be of type string.
+// If the item passed into the function is not in the store, then a statement informing the user of this should be logged to the console.
+
+// For Example:
+
+// itemPrice(“case”) should return the string $25.
+
+const itemPrice = function(item){
+    if (typeof item !== "string"){
+        return "All entries must be a string"
+    }
+
+    switch(item){
+        
+    }
+}
+
+let output = itemPrice("Hello")
+
+console.log(output);
---
+++ activities-generation/js-1js-2sba/challenge-3.js
@@ -0,0 +1,18 @@
+// In this challenge you will create a random restaurant generator.
+
+
+// Add the following restaurants to an array named restaurants.
+// Applebees
+// Panera Bread
+// Popeyes
+// Chopt
+// Chick-fil-A
+// Cheesecake Factory
+// Five Guys
+// Write a function called randomRestaurant that is defined with 1 parameter: numOfRestaurants.
+// The function will add the specified amount of restaurant names passed in (numOfRestaurants) from the restaurants array to another array called randomRestaurantList. It is ok if a restaurant appears in your list more than once.
+// Return the randomRestaurantList array.
+
+// For Example:
+
+// randomRestaurant(3) should return a randomRestaurantList array with the names of 3 random from the restaurants array.
